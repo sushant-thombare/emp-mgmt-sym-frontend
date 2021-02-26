60f7ee2aeaea7b639ec3f7cb52b85e7c2b60d336
sushant-thombare <me.sushant.thombare@gmail.com>
2021-02-26 22:36:53 +0530

    ListEmployee and CreateEmployee components completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
-import ListEmployeeComponent from './comopnents/ListEmployeeComponent';
+import "./App.css";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import FooterComponent from "./components/FooterComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import CreateEmployeeComponent from "./components/CreateEmployeeComponent";
 
 function App() {
   return (
-    <div className="container">
-      <ListEmployeeComponent />
+    <div>
+      <Router>        
+          <HeaderComponent />
+          <div className="container">
+            <Switch>
+              <Route path = "/" exact component = {ListEmployeeComponent}></Route>
+              <Route path = "/employees" component = {ListEmployeeComponent}></Route>
+              <Route path = "/add-employee" component = {CreateEmployeeComponent}></Route>              
+            </Switch>
+          </div>
+          <FooterComponent />       
+      </Router>
     </div>
   );
 }
--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import EmployeeService from '../services/EmployeeService';
 
 class ListEmployeeComponent extends Component {
     
@@ -7,44 +8,52 @@ class ListEmployeeComponent extends Component {
         this.state = {
             employees: []
         }
+        this.addEmployee = this.addEmployee.bind(this);
+    }
+
+    componentDidMount() {
+        EmployeeService.getEmployees()
+            .then(res => {
+                this.setState({employees: res.data});
+            })
+    }
+
+    addEmployee() {
+        this.props.history.push('/add-employee');
     }
     
     render() {
         return (
-            <div>
-                
+            <div>                
                 <h2 className="text-center">Employees List</h2>
-
                 <div className="row">
-                    
-                    <table className="table table-striped table-bordered">
-                        
+                    <button className="btn btn-primary" onClick={this.addEmployee}>Add Employee</button>
+                </div>
+                <div className="row">                    
+                    <table className="table table-striped table-bordered">                        
                         <thead>
                             <tr>
+                                <th>ID</th>
                                 <th>First Name</th>
                                 <th>Last Name</th>
                                 <th>Email ID</th>
                                 <th>Actions</th>
                             </tr>
                         </thead>
-
                         <tbody>
-
                             {
                                 this.state.employees.map(employee => (
                                     <tr key = {employee.id}>
+                                        <td>{employee.id}</td>
                                         <td>{employee.firstName}</td>
                                         <td>{employee.lastName}</td>
                                         <td>{employee.emailId}</td>
                                     </tr>
                                 ))
                             }
-
                         </tbody>
                     </table>
-
                 </div>
-
             </div>
         );
     }
