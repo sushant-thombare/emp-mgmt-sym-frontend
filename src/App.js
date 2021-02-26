62f6558f90cacc704f84f161cdac91e0c616eb87
sushant-thombare <me.sushant.thombare@gmail.com>
2021-02-26 19:27:14 +0530

    Initial Commit with Bootstrap Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ListEmployeeComponent from './comopnents/ListEmployeeComponent';
+
+function App() {
+  return (
+    <div className="container">
+      <ListEmployeeComponent />
+    </div>
+  );
+}
+
+export default App;
